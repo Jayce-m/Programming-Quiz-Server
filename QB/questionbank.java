58b4e28ad1a64df39d7bf5aa2955886a4391618a
oobiwanjanodi <111980342+oobiwanjanodi@users.noreply.github.com>
2023-04-21 17:05:28 +0800

    parsing code

--- QB/questionbank.java
+++ QB/questionbank.java
@@ -1,6 +1,24 @@
 import java.util.*;
+import java.io.FileReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 public class QuestionBank{
 
-    
+    public static void main(String[] args) {
+        try (JsonReader reader = Json.createReader(new FileReader("data.json"))) {
+            JsonObject jsonObj = reader.readObject();
+            String name = jsonObj.getString("name");
+            int age = jsonObj.getInt("age");
+            boolean isMarried = jsonObj.getBoolean("isMarried");
+
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Married: " + isMarried);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
