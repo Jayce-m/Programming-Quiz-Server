85c3064ef0898f8389d06758cf9d28383fae834c
oobiwanjanodi <111980342+oobiwanjanodi@users.noreply.github.com>
2023-04-21 17:51:26 +0800

    trying to get it

--- QB/questionbank.java
+++ QB/questionbank.java
@@ -1,28 +1,60 @@
-import java.util.*;
 import java.io.FileReader;
+import java.io.IOException;
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
+import java.util.*;
+
 
 public class questionbank{
 
-    public static void main(String[] args) {
-        try (JsonReader reader = Json.createReader(new FileReader("questions.json"))) {
-            JsonObject jsonObj = reader.readObject();
+    public class JsonParserExample {
+        public static void main(String[] args) {
+            try {
+                // Create a JsonReader object and pass the file reader to it
+                JsonReader reader = Json.createReader(new FileReader("questions.json"));
 
+                // Get the JSON object from the reader
+                JsonObject json = reader.readObject();
+
+                // Get the "questions" array from the JSON object
+                JsonArray questions = json.getJsonArray("questions");
+
+                //select a random question
+                String[] testQuestion = new String[10];
             
-            /* 
-            String name = jsonObj.getString("name");
-            int age = jsonObj.getInt("age");
-            boolean isMarried = jsonObj.getBoolean("isMarried");
+                for(int i = 0; i<testQuestion.length; i++){
+                    testQuestion[i] = (String) questions.get("question");
+                    
+                    int questionIndex = (int) (Math.random() * questions.size());
+                    JsonObject question = questions.getJsonObject(questionIndex);
+                    testQuestion[i] = question.getInt("question");
 
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-            System.out.println("Married: " + isMarried);
-        } catch (Exception e) {
-            e.printStackTrace();
+                }
+                
+                // Get the question text
+
+
+                // Iterate through each question in the array
+                for (int i = 0; i < questions.size(); i++) {
+                    JsonObject question = questions.getJsonObject(i);
+
+                    // Get the values of the question's fields
+                    int id = question.getInt("id");
+                    String text = question.getString("text");
+
+                    // Do something with the question's values
+                    // ...
+                }
+
+                // Close the reader
+                reader.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        */
     }
-
 }
+
